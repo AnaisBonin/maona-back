606bb8e5b2a1e57ddca78876e26443b3d13e3587
AnaisBonin <anaisboninfr@gmail.com>
2022-02-03 02:18:26 +0100

    Edited reviews route

--- models/reviewsModel.js
+++ models/reviewsModel.js
@@ -3,7 +3,7 @@ const connection = require('../db-config');
 const db = connection.promise();
 
 const findAllReviews = ({ productId }) => {
-  let query = 'SELECT * FROM reviews';
+  let query = 'SELECT r.*, u.firstname FROM reviews r JOIN users u ON u.id = r.userId';
   const params = [];
 
   if (productId) {
